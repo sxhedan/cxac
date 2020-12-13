e5cb1ff06871533cd86ea0616b59117bba8cda1b
dshe <sxhedan@gmail.com>
2020-12-12 20:36:15 -0500

    Tested fetch.

--- src/pages/Test/index.js
+++ src/pages/Test/index.js
@@ -1,7 +1,46 @@
+import { useEffect, useState } from "react";
+
 function Test() {
+  const [state, setState] = useState({
+    count: 0,
+    description: "Empty"
+  });
+  
+  useEffect(() => {
+    setState(prevState => ({
+      ...prevState,
+      count: 1
+    }));
+
+    const fetchDescription = async () => {
+      await fetch("https://azuredragon.noopsbycertik.com/shield/status", {
+        method: "GET"
+      })
+        .then(res => res.json())
+        .then(res => {
+          setState(prevState => ({
+            ...prevState,
+            description: JSON.stringify(res)
+          }));
+        },
+        err => {
+          setState(prevState => ({
+            ...prevState,
+            description: "failed"
+          }));
+        });
+    };
+    fetchDescription();
+  }, []);
+
   return (
     <div>
-      test
+      <div class="container">
+        Test: {state.count}
+      </div>
+      <div class="container">
+        Description: {state.description}
+      </div>
     </div>
   );
 }
